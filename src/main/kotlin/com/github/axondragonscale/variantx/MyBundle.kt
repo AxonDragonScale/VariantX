0b7fed98cb14f2c4de9aaa685d348ceca6ca7f9d
GitHub Action <action@github.com>
2026-03-19 09:39:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/axondragonscale/variantx/MyBundle.kt
+++ src/main/kotlin/com/github/axondragonscale/variantx/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.axondragonscale.variantx
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/axondragonscale/variantx/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/axondragonscale/variantx/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.axondragonscale.variantx.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
